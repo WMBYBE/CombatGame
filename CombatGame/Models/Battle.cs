c562cd8b41a3db8ca1213cd076973b54123c074e
WMBYBE <williambbrouillette@gmail.com>
2024-12-04 10:29:07 -0600

    add hp and moves to character model

--- CombatGame/Models/Battle.cs
+++ CombatGame/Models/Battle.cs
@@ -8,6 +8,7 @@
         public Team Team1 {  get; set; }
         public Team Team2 { get; set; }
         public string Result { get; set; }
-
+        //thinking maybe its team v team and it takes the combined damage of a team against the combined hp 
+        //of the opponent per turn, maybe with some randomness to it.
     }
 }
--- CombatGame/Models/Character.cs
+++ CombatGame/Models/Character.cs
@@ -4,9 +4,11 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int HP { get; set; }
         public int Strength { get; set; }
         public int Agility { get; set; }
         public int Intelligence { get; set; }
+        public List<Moves> Moves { get; set; } = new List<Moves>();
         //not sure if we should do characters are added directly to a team or you make characters and
         //can select to add them to a team
         //public int teamId { get; set; }
