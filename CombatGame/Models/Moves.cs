80abbd3c682be7c09de6c405f2e2f39141e8a751
WMBYBE <williambbrouillette@gmail.com>
2024-12-04 10:25:51 -0600

    added a moves model

---
+++ CombatGame/Models/Moves.cs
@@ -0,0 +1,13 @@
+﻿namespace CombatGame.Models
+{
+    public class Moves
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; } //scaling on strength or int?
+        public double Power { get; set; } //This is like the multiplier on the attack stat
+        public int speed { get; set; } //Strong attacks should attack slower than weaker ones? 
+
+    }
+}
